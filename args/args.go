2aedd3dec1fd54ef94dac8be8a7b3d0b9eff8c28
fujiokayu <fujiokayu@gmail.com>
2019-05-07 21:45:04 +0900

    Add Crypto Mode

--- args/args.go
+++ args/args.go
@@ -1,24 +1,39 @@
 package args
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 // Args struct
 type Args struct {
-	Key string
-	Msg string
+	EncryptMode bool
+	DecryptMode bool
+	Key         string
+	Msg         string
 }
 
 // ParseArgs : constructor of struct "args"
 func ParseArgs() *Args {
 
-	arg1 := flag.String("key", "default", "byte key")
-	arg2 := flag.String("msg", "default", "message to crypto")
+	arg1 := flag.Bool("e", false, "encrypt mode")
+	arg2 := flag.Bool("d", false, "decrypt mode")
+	arg3 := flag.String("key", "default", "byte key")
+	arg4 := flag.String("msg", "default", "message to crypto")
 
 	flag.Parse()
 
 	newArgs := &Args{
-		Key: *arg1,
-		Msg: *arg2,
+		EncryptMode: *arg1,
+		DecryptMode: *arg2,
+		Key:         *arg3,
+		Msg:         *arg4,
+	}
+
+	if newArgs.EncryptMode == newArgs.DecryptMode {
+		fmt.Println("have to specify Encrypt Mode or Decrypt Mode.")
+		os.Exit(1)
 	}
 	return newArgs
 }
